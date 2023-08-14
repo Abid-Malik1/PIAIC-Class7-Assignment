351ce8983a2d35ccace1a0761797ca954eeaff82
Malik Abid <malikabidzz0306@gmail.com>
2023-08-14 14:13:08 +0500

    PIAIC-Assignment

---
+++ 5table.js
@@ -0,0 +1,7 @@
+var number = 5;
+var multiplier = 1;
+while (multiplier <= 10) {
+    var result = number * multiplier;
+    console.log(number + " x " + multiplier + " = " + result);
+    multiplier++;
+}
---
+++ 5table.ts
@@ -0,0 +1,8 @@
+let number = 5;
+let multiplier = 1;
+
+while (multiplier <= 10) {
+    let result:number = number * multiplier;
+    console.log(number + " x " + multiplier + " = " + result);
+    multiplier++;
+}
